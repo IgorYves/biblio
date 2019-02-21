563c0bef5333e8e7e8e914b65814c9990fb40c4e
admin <admin@Client16>
2019-02-21 08:50:47 +0100

    3 essais

--- biblio2/src/main/java/bibliotheque/biblio2/App.java
+++ biblio2/src/main/java/bibliotheque/biblio2/App.java
@@ -4,10 +4,9 @@ package bibliotheque.biblio2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println();
+	}
 }
